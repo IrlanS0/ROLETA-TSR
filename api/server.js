31c31f7a78be3eeaead38815dff8d4bea34a3543
Irlan <irlanfelipe@academico.ufs.br>
2026-09-08 00:22:55 -0300

    Refactor CORS configuration for Socket.IO server

--- api/server.js
+++ api/server.js
@@ -5,7 +5,12 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: 'https://roleta-tsr.vercel.app/', methods: ['GET', 'POST'] } });
+const io = new Server(server, {
+  cors: {
+    origin: "https://roleta-tsr.vercel.app",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static(path.join(process.cwd(), 'public')));
 
